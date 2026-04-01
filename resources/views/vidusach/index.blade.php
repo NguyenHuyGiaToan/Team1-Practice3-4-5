b3c334253751dae95a63450ae20234c6ae391ca2
NguyenMinh241105 <nguyentrongminhh2005@gmail.com>
2026-04-01 23:19:04 +0700

    sửa lỗi giao diện

--- resources/views/vidusach/index.blade.php
+++ resources/views/vidusach/index.blade.php
@@ -1,44 +1,48 @@
 <x-book-layout>
-<x-slot name='title'>
-    Sách
-</x-slot>
-<div class='list-book'>
-  @foreach($data as $row)
-    <div class='book'> 
-        <a href="{{url('sach/chitiet/'.$row->id)}}">
-            <img src="{{asset('storage/book_image/'.$row->file_anh_bia)}}" width='200px' height='200px'><br>
-            <b>{{$row->tieu_de}}</b><br/>
-            <i>{{number_format($row->gia_ban,0,",",".")}}đ</i>
-        </a>
+    <x-slot name='title'>
+        Sách
+    </x-slot>
+    <div class='list-book'>
+        @foreach($data as $row)
+        <div class='book'>
+            <a href="{{url('sach/chitiet/'.$row->id)}}">
+                <img src="{{asset('hinh/image/'.$row->file_anh_bia)}}" width='200px' height='200px'><br>
+                <b>{{$row->tieu_de}}</b><br />
+                <i>{{number_format($row->gia_ban,0,",",".")}}đ</i>
+                <div class='btn-add-product'>
+                    <button class='btn btn-success btn-sm mb-1 add-product' book_id="{{$row->id}}">
+                        Thêm vào giỏ hàng
+                    </button>
+
+                </div>
+            </a>
+        </div>
+
+
+        @endforeach
     </div>
-<div class='btn-add-product'>
-<button class='btn btn-success btn-sm mb-1 add-product' book_id="{{$row->id}}">
-Thêm vào giỏ hàng
-</button>
-<script>
-$(document).ready(function(){
-$(".add-product").click(function(){
-id = $(this).attr("book_id");
-num = 1;
-$.ajax({
-type:"POST",
-dataType:"json",
-url: "{{route('cartadd')}}",
-data:{"_token": "{{ csrf_token() }}","id":id,"num":num},
-beforeSend:function(){
-},
-success:function(data){
-$("#cart-number-product").html(data);
-},
-error: function (xhr,status,error){
-},
-complete: function(xhr,status){
-}
-});
-});
-});
-</script>
-</div>
-  @endforeach
-</div>
+    <script>
+        $(document).ready(function() {
+            $(".add-product").click(function() {
+                id = $(this).attr("book_id");
+                num = 1;
+                $.ajax({
+                    type: "POST",
+                    dataType: "json",
+                    url: "{{route('cartadd')}}",
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        "id": id,
+                        "num": num
+                    },
+                    beforeSend: function() {},
+                    success: function(data) {
+                        $("#cart-number-product").html(data);
+                    },
+                    error: function(xhr, status, error) {},
+                    complete: function(xhr, status) {}
+                });
+            });
+        });
+    </script>
 </x-book-layout>
